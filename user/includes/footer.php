6d070bc1300440caf666026bb931a34a6902a088
Priyanka Galave <137290021+priyanka-2502@users.noreply.github.com>
2024-08-11 22:24:55 +0530

    Add files via upload

---
+++ user/includes/footer.php
@@ -0,0 +1,20 @@
+<footer style = "background-color:black; margin-top:0%;width:113%;height:50%;">
+<br>
+<h3 style="color:white;text-align:center;"> contact us through social media</h3><br>
+<div style="margin:0px 570px;">
+<a href="#" class="fa fa-facebook"></a>
+<a href="#" class="fa fa-twitter"></a>
+<a href="#" class="fa fa-google"></a>
+<a href="#" class="fa fa-instagram"></a>
+<a href="#" class="fa fa-yahoo"></a>
+</div>
+<p style="color:white;text-align:center;"><br>
+    Email: &nbsp online.beauty@gmail.com <br><br>
+    Mobile: &nbsp +91 8669520034<br>
+    made by:-priya-minal
+</p>
+
+</footer>
+</div>
+</body>
+</html>
---
+++ user/includes/header.php
@@ -0,0 +1,57 @@
+
+<?php
+include('includes/dbconnection.php');
+//include('partials/login-check.php')
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>online beauty</title>
+
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" type="text/css"  href="../CSS/home.css">
+
+</head>
+<style type="text/css">
+
+
+</style>
+<body>
+<div class="wrapper">
+    <header>
+        <div class="logo">
+            <h1>BEAUTY SALON</h1>
+        </div>
+    <nav>
+        <ul>
+            <li><a href="user_home.php">HOME</a></li>
+            <li><a href="services.php"> SERVICES</a></li>
+            <li><a href="book_appoint.php">BOOK-APPOINTMENT</a></li>
+            <li><a href="book_history.php">HISTORY</a></li>
+            <li><a href="invoice_history.php">INVOICE</a></li>
+            <li><a href="logout.php">LOG-OUT</a></li>
+            
+        </ul>
+    </nav>
+
+
+
+    </header>
